56f028fce258e4a243246627ef57ba1143bbb30f
Maksim Mogachev <mogachevmaksim@gmail.com>
2021-03-05 22:45:17 +0300

    resolve conflict, v2.0

--- src/main/java/com/senlacourses/lecture3/task3/assemblyLine/IAssemblyLine.java
+++
@@ -1,9 +0,0 @@
-package com.senlacourses.lecture3.task3.assemblyLine;
-
-import com.senlacourses.lecture3.task3.product.IProduct;
-
-public interface IAssemblyLine {
-
-  IProduct assembleProduct(IProduct iProduct);
-
-}

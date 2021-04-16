184661ed15bc499d5e6919cbc5a2949fc7acb4b3
Maksim Mogachev <mogachevmaksim@gmail.com>
2021-04-16 23:48:06 +0300

    fix bugs v2

--- src/main/java/com/senlacourses/electronicHotelAdministrator/domain/model/HotelResident.java
+++ src/main/java/com/senlacourses/electronicHotelAdministrator/domain/model/HotelResident.java
@@ -1,12 +1,8 @@
 package com.senlacourses.electronicHotelAdministrator.domain.model;
 
-import java.io.Serial;
 import java.io.Serializable;
 
 public record HotelResident(String fullName, int passportNumber) implements Serializable {
-
-  @Serial
-  private static final long serialVersionUID = -3712898207284411440L;
 
   @Override
   public String toString() {
--- src/main/java/com/senlacourses/electronicHotelAdministrator/ui/Menu.java
+++ src/main/java/com/senlacourses/electronicHotelAdministrator/ui/Menu.java
@@ -1,45 +1,5 @@
 package com.senlacourses.electronicHotelAdministrator.ui;
 
-import com.senlacourses.electronicHotelAdministrator.domain.controller.HotelResidentController;
-import com.senlacourses.electronicHotelAdministrator.domain.controller.HotelRoomController;
-import com.senlacourses.electronicHotelAdministrator.domain.controller.RegistrationCardController;
-import com.senlacourses.electronicHotelAdministrator.domain.controller.ServiceController;
-import com.senlacourses.electronicHotelAdministrator.domain.controller.interfaces.IHotelResidentController;
-import com.senlacourses.electronicHotelAdministrator.domain.controller.interfaces.IHotelRoomController;
-import com.senlacourses.electronicHotelAdministrator.domain.controller.interfaces.IRegistrationCardController;
-import com.senlacourses.electronicHotelAdministrator.domain.controller.interfaces.IServiceController;
-import com.senlacourses.electronicHotelAdministrator.domain.service.interfaces.IHotelResidentService;
-import com.senlacourses.electronicHotelAdministrator.domain.service.interfaces.IHotelRoomService;
-import com.senlacourses.electronicHotelAdministrator.domain.service.interfaces.IRegistrationCardService;
-import com.senlacourses.electronicHotelAdministrator.domain.service.interfaces.IServiceService;
-import com.senlacourses.electronicHotelAdministrator.ui.actions.hotelResident.AddNewResidentAction;
-import com.senlacourses.electronicHotelAdministrator.ui.actions.hotelResident.RemoveResidentAction;
-import com.senlacourses.electronicHotelAdministrator.ui.actions.hotelResident.ShowAllResidentsAction;
-import com.senlacourses.electronicHotelAdministrator.ui.actions.hotelRoom.AddNewRoomAction;
-import com.senlacourses.electronicHotelAdministrator.ui.actions.hotelRoom.ChangeRoomConditionAction;
-import com.senlacourses.electronicHotelAdministrator.ui.actions.hotelRoom.ChangeRoomPriceAction;
-import com.senlacourses.electronicHotelAdministrator.ui.actions.hotelRoom.ShowAllRoomsAction;
-import com.senlacourses.electronicHotelAdministrator.ui.actions.hotelRoom.ShowAllRoomsByCriterionAction;
-import com.senlacourses.electronicHotelAdministrator.ui.actions.hotelRoom.ShowFreeRoomsByCriterionAction;
-import com.senlacourses.electronicHotelAdministrator.ui.actions.hotelRoom.ShowLastResidentsOfRoomAction;
-import com.senlacourses.electronicHotelAdministrator.ui.actions.hotelRoom.ShowNumberOfFreeRoomsAction;
-import com.senlacourses.electronicHotelAdministrator.ui.actions.hotelRoom.ShowRoomDetailsAction;
-import com.senlacourses.electronicHotelAdministrator.ui.actions.hotelRoom.ShowRoomsByDateAction;
-import com.senlacourses.electronicHotelAdministrator.ui.actions.residentCard.AddServiceToOccupiedRoomAction;
-import com.senlacourses.electronicHotelAdministrator.ui.actions.residentCard.EvictFromTheRoomAction;
-import com.senlacourses.electronicHotelAdministrator.ui.actions.residentCard.PutInTheRoomActon;
-import com.senlacourses.electronicHotelAdministrator.ui.actions.residentCard.ShowAmountOfPayment;
-import com.senlacourses.electronicHotelAdministrator.ui.actions.residentCard.ShowNumberOfCurrentResidentsAction;
-import com.senlacourses.electronicHotelAdministrator.ui.actions.residentCard.ShowOccupiedRoomByCriterionAction;
-import com.senlacourses.electronicHotelAdministrator.ui.actions.residentCard.ShowOccupiedRoomsAction;
-import com.senlacourses.electronicHotelAdministrator.ui.actions.residentCard.ShowResidentServicesByCriterion;
-import com.senlacourses.electronicHotelAdministrator.ui.actions.service.AddNewServiceAction;
-import com.senlacourses.electronicHotelAdministrator.ui.actions.service.ChangeServicePriceAction;
-import com.senlacourses.electronicHotelAdministrator.ui.actions.service.ShowCurrentServicesAction;
-import com.senlacourses.electronicHotelAdministrator.ui.actions.service.ShowPriceOfServicesAndRoomsByCriterionAction;
-import java.util.ArrayList;
-import java.util.List;
-
 public class Menu {
 
   private String name;
@@ -74,7 +34,6 @@ public class Menu {
 
     public Builder() {}
 
-
     public Builder name(String name) {
       this.name = name;
       return this;
@@ -88,7 +47,5 @@ public class Menu {
     public Menu build() {
       return new Menu(this.name, this.menuItems);
     }
-
-
   }
 }

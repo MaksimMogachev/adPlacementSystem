93dcbac7c8a2e8858151ad9b91e8a8656cf5757b
Maksim_Mogachev <mogachevmaksim@gmail.com>
2021-05-27 20:42:06 +0300

    refactored multithreading, v2

--- src/main/java/com/senlacourses/electronicHotelAdministrator/multithreading/taskOne/TaskOne.java
+++ src/main/java/com/senlacourses/electronicHotelAdministrator/multithreading/taskOne/TaskOne.java
@@ -1,38 +1,26 @@
 package com.senlacourses.electronicHotelAdministrator.multithreading.taskOne;
 
-public class TaskOne implements Runnable {
+public class TaskOne {
 
-  Thread main = Thread.currentThread();
+  static Thread main = Thread.currentThread();
 
   public static void main(String[] args) throws InterruptedException {
-    TaskOne taskOne = new TaskOne();
     TaskOneHelper taskOneHelper = new TaskOneHelper();
     Thread thread1 = new Thread(taskOneHelper);
-    Thread thread2 = new Thread(taskOne);
 
     printState(thread1);
     thread1.start();
     printState(thread1);
-    thread2.start();
 
     Thread.sleep(1000);
     printState(thread1);
     taskOneHelper.notifier();
     printState(thread1);
-    printState(thread2);
+    Thread.sleep(1000);
+    printState(thread1);
   }
 
-  private static void printState(Thread thread) {
+  protected static void printState(Thread thread) {
     System.out.println(thread.getState());
-  }
-
-  @Override
-  public void run() {
-    try {
-      Thread.sleep(100);
-      printState(main);
-    } catch (InterruptedException e) {
-      e.printStackTrace();
-    }
   }
 }
--- src/main/java/com/senlacourses/electronicHotelAdministrator/multithreading/taskOne/TaskOneHelper.java
+++ src/main/java/com/senlacourses/electronicHotelAdministrator/multithreading/taskOne/TaskOneHelper.java
@@ -1,9 +1,13 @@
 package com.senlacourses.electronicHotelAdministrator.multithreading.taskOne;
 
+import static com.senlacourses.electronicHotelAdministrator.multithreading.taskOne.TaskOne.printState;
+
 public class TaskOneHelper implements Runnable {
   @Override
   public synchronized void run() {
     try {
+      Thread.sleep(100);
+      printState(TaskOne.main);
       wait();
     } catch (InterruptedException e) {
       e.printStackTrace();
